6ca5251cfff684e888f8fb3010ffaf3a0f78eaed
SI-86 <samaldenhren@gmail.com>
2022-10-19 21:57:04 -0400

    basic movement

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -5,10 +5,12 @@ import { Mesh, Vector3 } from "three";
 import { useKeyboard } from "../hooks/useKeyboard";
 
 const JUMP_HEIGHT = 4;
+const SPEED = 4;
 
 export const Player = () => {
     // handle keyboard input
-    const actions = useKeyboard();
+    const { moveBackward, moveForward, moveLeft, moveRight, jump } =
+        useKeyboard();
 
     // get the camera and set the position
     const { camera } = useThree();
@@ -39,8 +41,30 @@ export const Player = () => {
             new Vector3(pos.current[0], pos.current[1], pos.current[2])
         );
 
+        const direction = new Vector3();
+
+        const frontVector = new Vector3(
+            0,
+            0,
+            (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+        );
+
+        const sideVector = new Vector3(
+            (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+            0,
+            0
+        );
+
+        direction
+            .subVectors(frontVector, sideVector)
+            .normalize()
+            .multiplyScalar(SPEED)
+            .applyEuler(camera.rotation);
+
+        api.velocity.set(direction.x, vel.current[1], direction.z);
+
         // Move the player
-        if (actions.jump && Math.abs(vel.current[1]) < 0.05) {
+        if (jump && Math.abs(vel.current[1]) < 0.05) {
             api.velocity.set(vel.current[0], JUMP_HEIGHT, vel.current[2]);
         }
     });
